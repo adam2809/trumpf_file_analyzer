56b8110c1a516ce3da7ada69cee7516c528e1a56
Adam Kulesza <adam.kule@gmail.com>
2025-11-11 22:59:52 +0100

    Adds double deque header file

--- src/static_double_deque.h
+++ src/static_double_deque.h
@@ -1,4 +1,22 @@
 #ifndef STATIC_DOUBLE_DEQUE_H
 #define STATIC_DOUBLE_DEQUE_H
 
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct static_double_deque_t {
+    uint16_t front_index;
+    uint16_t back_index;
+    uint16_t max_size;
+    double* buf;
+} static_double_deque_t;
+
+bool static_double_deque_init(static_double_deque_t* queue, double* buf, uint16_t max_size);
+bool static_double_deque_push_front(static_double_deque_t* queue, double value);
+bool static_double_deque_pop_front(static_double_deque_t* queue, double* value);
+bool static_double_deque_push_back(static_double_deque_t* queue, double value);
+bool static_double_deque_pop_back(static_double_deque_t* queue, double* value);
+void print_queue(static_double_deque_t* queue);
+
 #endif
