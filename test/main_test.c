4ce207629197efa34eabcaed2eee4299daf4ebb6
Adam Kulesza <adam.kule@gmail.com>
2025-11-12 00:17:54 +0100

    Tests crossing over ring buffer seam in deque

--- test/main_test.c
+++ test/main_test.c
@@ -3,26 +3,29 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define TEST_QUEUE_SIZE 6
 
 void test_fill_and_empty_queue_front(void);
 void test_fill_and_empty_queue_back(void);
+void test_queue_crossing_ring_buffer_seam(void);
 
 int main(int argc, const char *argv[]) {
   test_fill_and_empty_queue_front();
   test_fill_and_empty_queue_back();
+  test_queue_crossing_ring_buffer_seam();
+
 
   printf("All tests passed!\n");
 }
 void test_fill_and_empty_queue_front(void) {
   printf("Test fill to max and empty all from front\n");
-  double buffer[TEST_QUEUE_SIZE];
+  int test_queue_size = 6;
+  double buffer[test_queue_size];
   static_double_deque_t queue;
 
-  assert(!static_double_deque_init(&queue, NULL, TEST_QUEUE_SIZE));
-  assert(!static_double_deque_init(NULL, buffer, TEST_QUEUE_SIZE));
+  assert(!static_double_deque_init(&queue, NULL, test_queue_size));
+  assert(!static_double_deque_init(NULL, buffer, test_queue_size));
 
-  assert(static_double_deque_init(&queue, buffer, TEST_QUEUE_SIZE));
+  assert(static_double_deque_init(&queue, buffer, test_queue_size));
 
   assert(queue.front_index == 0);
   assert(queue.back_index == 0);
@@ -67,13 +70,14 @@ void test_fill_and_empty_queue_front(void) {
 
 void test_fill_and_empty_queue_back(void) {
   printf("Test fill to max and empty all from back\n");
-  double buffer[TEST_QUEUE_SIZE];
+  int test_queue_size = 6;
+  double buffer[test_queue_size];
   static_double_deque_t queue;
 
-  assert(!static_double_deque_init(&queue, NULL, TEST_QUEUE_SIZE));
-  assert(!static_double_deque_init(NULL, buffer, TEST_QUEUE_SIZE));
+  assert(!static_double_deque_init(&queue, NULL, test_queue_size));
+  assert(!static_double_deque_init(NULL, buffer, test_queue_size));
 
-  assert(static_double_deque_init(&queue, buffer, TEST_QUEUE_SIZE));
+  assert(static_double_deque_init(&queue, buffer, test_queue_size));
 
   assert(queue.front_index == 0);
   assert(queue.back_index == 0);
@@ -114,4 +118,41 @@ void test_fill_and_empty_queue_back(void) {
   assert(queue.size == 0);
   assert(queue.front_index == 0);
   assert(queue.back_index == 0);
+}
+
+void test_queue_crossing_ring_buffer_seam(void){
+  printf("Test popping and puhsing while crossing over ring buffer seam that the queue uses\n");
+
+  int test_queue_size = 3;
+  double buffer[test_queue_size];
+  static_double_deque_t queue;
+
+  assert(static_double_deque_init(&queue, buffer, test_queue_size));
+  assert(static_double_deque_push_front(&queue, 1));
+  assert(static_double_deque_push_front(&queue, 2));
+
+  double value;
+  assert(static_double_deque_pop_back(&queue, &value));
+  assert(value == 1);
+  assert(queue.front_index == 1);
+  assert(queue.back_index == 1);
+
+  assert(static_double_deque_push_front(&queue, 3));
+  assert(static_double_deque_push_front(&queue, 1));
+  assert(!static_double_deque_push_front(&queue, 4));
+
+  assert(queue.back_index == 1);
+  assert(queue.front_index == 0);
+
+  assert(static_double_deque_pop_back(&queue, &value));
+  assert(value == 2);
+  assert(static_double_deque_pop_back(&queue, &value));
+  assert(value == 3);
+  assert(static_double_deque_push_back(&queue, 4));
+
+  assert(queue.back_index == 2);
+  assert(queue.front_index == 0);
+  assert(buffer[queue.back_index] == 4);
+  assert(buffer[queue.front_index] == 1);
+  assert(queue.size == 2);
 }
