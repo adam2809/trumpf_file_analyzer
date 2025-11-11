c1d4d8813f0b64f4eff51ebb52200683653c2a6a
Adam Kulesza <adam.kule@gmail.com>
2025-11-11 23:07:35 +0100

    Adds init tests

--- test/main_test.c
+++ test/main_test.c
@@ -3,6 +3,19 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define TEST_QUEUE_SIZE 6
+
 int main(int argc, const char *argv[]) {
-  assert(false);
+  double buffer[TEST_QUEUE_SIZE];
+  static_double_deque_t queue;
+
+  assert(!static_double_deque_init(&queue, NULL, TEST_QUEUE_SIZE));
+  assert(!static_double_deque_init(NULL, buffer, TEST_QUEUE_SIZE));
+
+  assert(static_double_deque_init(&queue, buffer, TEST_QUEUE_SIZE));
+
+  assert(queue.front_index == 0);
+  assert(queue.back_index == 0);
+
+  printf("All tests passed!\n");
 }
