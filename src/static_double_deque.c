c4fc576da096ea623125d47fe7c0732a2b44a320
Adam Kulesza <adam.kule@gmail.com>
2025-11-11 23:07:47 +0100

    Implements deque init function

--- src/static_double_deque.c
+++ src/static_double_deque.c
@@ -1,1 +1,14 @@
 #include "static_double_deque.h"
+#include <stddef.h>
+
+bool static_double_deque_init(static_double_deque_t* queue, double* buf, uint16_t max_size){
+    if (queue == NULL || buf == NULL || max_size == 0) {
+        return false;
+    }
+    queue->front_index = 0;
+    queue->back_index = 0;
+    queue->max_size = max_size;
+    queue->size = 0;
+    queue->buf = buf;
+    return true;
+}
