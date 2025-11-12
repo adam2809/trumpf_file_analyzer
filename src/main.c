beb7f9aaddbfc82a787ffdf9e1981179cfd6c4d9
Adam Kulesza <adam.kule@gmail.com>
2025-11-12 05:41:00 +0100

    Adds command line arguments reading

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
-#include "static_double_deque.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "file_analyzer.h"
+
+#define WINDOW_SIZE 500
+#define MAX_DOUBLE_STR_LEN 24
+
 
 int main(int argc, const char *argv[]) {
-  printf("Hello, World!\n");
+  if(argc != 4) {
+    printf("ERROR! Wrong arguments count. This program takes the input file directory as the first argument, output file directory as the second and window size as third\n");
+    return 1;
+  }
+
+  char *endptr;
+  uint16_t window_size = strtol((char *) argv[3], &endptr, 10);
+  if (*endptr != '\0') {
+    printf("ERROR! The provided window size in first argument is not a number\n");
+    return 1;
+  }
+
+  char* input_file_dir = (char *)argv[1];
+  char* output_file_dir = (char *)argv[2];
 }
