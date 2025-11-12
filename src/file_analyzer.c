b4cad661e70124aaa7d0670a0e5a6d1c39a6077c
Adam Kulesza <adam.kule@gmail.com>
2025-11-12 01:59:47 +0100

    Implements file analyzer init

--- src/file_analyzer.c
+++ src/file_analyzer.c
@@ -1,1 +1,25 @@
 #include "file_analyzer.h"
+
+bool file_analyzer_init(file_analyzer_ctx_t* ctx, 
+                        double* max_deque_buffer,
+                        double* min_deque_buffer,
+                        double* window_deque_buffer,
+                        uint16_t window_size) {
+    if (!ctx || !max_deque_buffer || !min_deque_buffer || !window_deque_buffer || window_size == 0) {
+        return false;
+    }
+
+    if (!static_double_deque_init(&ctx->max_deque, max_deque_buffer, window_size) ||
+        !static_double_deque_init(&ctx->min_deque, min_deque_buffer, window_size) ||
+        !static_double_deque_init(&ctx->window_deque, window_deque_buffer, window_size)) {
+        return false;
+    }
+
+    ctx->window_sum = 0.0;
+    ctx->window_size = window_size;
+
+    return true;
+}
+
+bool file_analyzer_process_value(file_analyzer_ctx_t* ctx, double value, double* max, double* min, double* avg) {
+}
